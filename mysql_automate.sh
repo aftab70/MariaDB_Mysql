2f0c17ddbc31adde5e45ff87da4f851d5aa2d9ce
aftab70 <43289912+aftab70@users.noreply.github.com>
2020-08-27 02:35:33 +0530

    Update mysql_automate.sh

--- mysql_automate.sh
+++ mysql_automate.sh
@@ -3,7 +3,7 @@
 apt-get update 
 apt-get install mysql-server -y
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';"
-sed -i '17s@bind-address            = 127.0.0.1@bind-address            = 0.0.0.0@' /etc/mysql/mysql.conf.d/mysqld.cnf
+sed -i '43s@bind-address            = 127.0.0.1@bind-address            = 0.0.0.0@' /etc/mysql/mysql.conf.d/mysqld.cnf
 systemctl restart mysql
 mysql -u root -ppassword -e "create database attendancedb;"
 mysql -u root -ppassword -e "show databases;"
