e5351ff58318f41e673e4385ae5c2fb4408668ab
aftab70 <43289912+aftab70@users.noreply.github.com>
2020-08-27 02:06:45 +0530

    Update mysql_automate.sh

--- mysql_automate.sh
+++ mysql_automate.sh
@@ -5,6 +5,10 @@ sudo apt-get install mysql-server -y
 sudo -i
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';"
 exit
+sudo sed -i '17s@bind-address            = 127.0.0.1@bind-address            = 0.0.0.0@' /etc/mysql/mysql.conf.d/mysqld.cnf
 sudo systemctl restart mysql
 mysql -u root -ppassword -e "create database attendancedb;"
 mysql -u root -ppassword -e "show databases;"
+mysql -u root -ppassword -e "CREATE USER 'root' IDENTIFIED BY 'password';"
+mysql -u root -ppassword -e "GRANT USAGE ON *.* TO 'root'@'%' IDENTIFIED BY 'password';"
+mysql -u root -ppassword -e "FLUSH PRIVILEGES;"
